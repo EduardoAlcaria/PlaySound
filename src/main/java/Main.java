0529ca6eff1e1bf46a83e62472a568f6c9eac4e8
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-05-13 09:09:46 -0300

    refactor(main) modulized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,74 +3,17 @@
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+import keydetector.GlobalHook;
 
 import javax.sound.sampled.*;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class Main implements NativeKeyListener {
 
-    private boolean shiftPressed = false;
-
     public static void main(String[] args) {
 
-        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
-        logger.setLevel(Level.OFF);
+       GlobalHook hook = new GlobalHook();
 
-        try {
-            GlobalScreen.registerNativeHook();
-        } catch (Exception e) {
-            System.err.println("Failed to register native hook.");
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        GlobalScreen.addNativeKeyListener(new Main());
-
-        System.out.println("Listening for ALT + ,");
-    }
-
-    @Override
-    public void nativeKeyPressed(NativeKeyEvent e) {
-
-        if (e.getKeyCode() == NativeKeyEvent.VC_ALT){
-            shiftPressed = true;
-        }
-
-
-        if (shiftPressed && e.getKeyCode() == NativeKeyEvent.VC_COMMA) {
-            playSound("/home/kaiser/Downloads/sound.wav");
-        }
-    }
-
-    @Override
-    public void nativeKeyReleased(NativeKeyEvent e) {
-        if (e.getKeyCode() == NativeKeyEvent.VC_ALT) {
-            shiftPressed = false;
-        }
-    }
-
-    @Override
-    public void nativeKeyTyped(NativeKeyEvent e) {
-    }
-
-    private static void playSound(String path) {
-        try {
-            File soundFile = new File(path);
-
-            AudioInputStream audioStream =
-                    AudioSystem.getAudioInputStream(soundFile);
-
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioStream);
-            clip.start();
-
-        } catch (UnsupportedAudioFileException |
-                 IOException |
-                 LineUnavailableException e) {
-            e.printStackTrace();
-        }
+       hook.addGlobalLister();
     }
 }
